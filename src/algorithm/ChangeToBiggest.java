b27c0edea54c3db21677f8c604f6b7b5c52bd7e1
Eddy.Liu <Eddy.liu@yff.com>
2022-09-14 10:21:20 +0800

    --taskId=202209141021 两数之和

---
+++ src/algorithm/ChangeToBiggest.java
@@ -0,0 +1,15 @@
+package algorithm;
+
+/**
+ * LC 670
+ * 修改 数字中的任意两位从而达到整个数最大
+ *
+ */
+public class ChangeToBiggest {
+
+    int changeToBiggest(int value) {
+        char[] valueArray = String.valueOf(value).toCharArray();
+
+        return 0;
+    }
+}
